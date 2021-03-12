f3339d9b9508c961df8ee4226ddc625c9540cff3
lualcs <1508786776@qq.com>
2021-03-12 07:32:21 -0800

    define.h

---
+++ common/macros/define.h
@@ -0,0 +1,38 @@
+/*
+    file:define.h
+    desc:定义
+    auth:Carol Luo
+*/
+
+//货币
+#define SCORE       INT64 //积分
+#define MASONRY     INT64 //砖石
+#define GOLD        INT64 //金币
+#define SIVLER      INT64 //银币
+#define COINS       INT64 //硬币
+#define COPPER      INT64 //铜币
+#define HOUSE       INT64 //房卡
+
+
+//扑克
+#define CARD        BYTE  //扑克
+#define ICON        BYTE  //图标
+#define COLOR       BYTE  //花色
+#define VALUE       BYTE  //牌值
+
+//字符
+#define NAME        CHAR[32]  //名字
+#define WECHAT      CHAR[32]  //微信
+#define QQ          CHAR[32]  //QQ号
+#define ACCOUNTS    CHAR[32]  //账号
+#define PHONE       CHAR[32]  //手机
+#define LOGO        CHAR[256] //头像
+#define URL         CHAR[256] //链接
+
+//标识
+#define PLATEID     INT32   //平台ID
+#define GAMEID      INT32   //游戏ID
+#define USERID      INT32   //用户ID
+#define ALLIANCEID  INT32   //联盟ID
+#define AGENDID     INT32   //代理ID
+#define MEMBERID    INT32   //成员ID
---
+++ common/structs/game.h
@@ -0,0 +1,7 @@
+/*
+    file:game.h
+    desc:游戏结构
+    auth:Carol Luo
+*/
+#include "../macros/define.h"
+
