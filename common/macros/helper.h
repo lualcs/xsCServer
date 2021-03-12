c2cd982e9489a2cc61a0e82ae90bb06d3a3d0b2d
lualcs <1508786776@qq.com>
2021-03-12 07:51:02 -0800

    card

---
+++ common/macros/helper.h
@@ -0,0 +1,9 @@
+/*
+    file:helper.h
+    desc:辅助
+    auth:Carol Luo
+*/
+
+#define getCard(COLOR,VALUE) ((COLOR<<4)|VALUE)    //合成牌
+#define getColor(CARD)       ((CARD&0xF0)>>4)      //取花色
+#define getValue(CARD)       (CARD&0x0F)           //取牌值
