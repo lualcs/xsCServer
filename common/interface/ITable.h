ffa6f34049b5596df9e009bbf231f7f00cfa181b
lualcs <1508786776@qq.com>
2021-03-13 08:22:52 -0800

    桌子接口

---
+++ common/interface/ITable.h
@@ -0,0 +1,18 @@
+/*
+    file:ITable.h
+    desc:桌子接口 
+    auth:Carol Luo
+*/
+
+//桌子接口
+class ITable {
+    public:
+    //获取桌子ID
+    virtual int getTableID();
+    //获取对局ID
+    virtual int getCombatID();
+    //获取位置数量
+    virtual int getMaxChair();
+    //获取玩家数量
+    virtual int getPlayCount();
+};
